4ee294829a402fe141033d9e507c9ce6b74f3510
Jakub Kojdecki <jakub@motorway.co.uk>
2022-06-09 20:31:01 +0200

    add fakes

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,15 @@ const {uploadDocument} = require("./src/use-cases/uploadDocument");
 const app = express();
 const body = require('body-parser');
 const {listProcessedDocuments, getProcessedDocument} = require("./src/use-cases/getProcessedDocuments");
+const {returnPostV5c} = require("./src/fakes/post_v5c");
+const {returnGetV5c, returnGetV5cId} = require("./src/fakes/get_v5c");
 const port = 3001;
 app.use(body.json());
 
 app.post('/v5c', async (req, res) => {
+    if (req.headers['x-get-fake']) {
+        return res.json(returnPostV5c());
+    }
     console.log('file uploaded', req.body);
     await uploadDocument(req.body);
     res.sendStatus(200);
@@ -15,11 +20,18 @@ app.post('/v5c', async (req, res) => {
 
 // Input only the filename, without prefix path
 app.get('/v5c/:id', async (req, res) => {
+    if (req.headers['x-get-fake']) {
+        console.log('fake it till you make it')
+        return res.json(returnGetV5cId());
+    }
     res.send(await getProcessedDocument(req.params.id));
 });
 
 // Lists processed documents
 app.get('/v5c', async (req, res) => {
+    if (req.headers['x-get-fake']) {
+        return res.json(returnGetV5c());
+    }
     res.send(await listProcessedDocuments());
 })
 
