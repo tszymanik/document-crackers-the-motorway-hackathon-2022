922edccf47b195d5790304998fe6259c976758d3
Michał Góra <michal@motorway.co.uk>
2022-06-09 22:05:24 +0200

    Add rotate module.

---
+++ backend/textract/analysis/rotate.js
@@ -0,0 +1,54 @@
+const rotatePoint = (x, y, angle) => {
+  const r = Math.sqrt((x * x) + (y * y));
+  const theta = Math.atan(y / x);
+  const newTheta = (theta - angle);
+  const newX = r * Math.cos(newTheta);
+  const newY = r * Math.sin(newTheta);
+  return {
+    x: newX,
+    y: newY,
+  }
+}
+
+const rotate = (blocks, angle) => blocks.slice(0,1).map(block => {
+  const polygon = block?.Geometry?.Polygon?.map?.(coordinate => {
+    const rotated = rotatePoint(coordinate.X, coordinate.Y, angle);
+    return {
+      X: rotated.x,
+      Y: rotated.y,
+    }
+  })
+
+  return {
+    ...block,
+    Geometry: {
+      ...block.Geometry,
+      Polygon: polygon
+    }
+  }
+})
+
+const blocks = require('./../results/v5-1_rotated_40_deg.png.json');
+const blocksOrg = require('./../results/v5-1.png.json');
+
+// original
+// 0.5996101498603821
+// 1
+
+// rotated 40deg clockwise
+// x 1.10212
+// y 0.380622
+
+// const rotatedBlocks = rotate([{Geometry:{
+//   Polygon:[
+//     {
+//       X: 1.10212,
+//       Y: 0.380622
+//     }
+//   ]
+// }}], -0.6981317007977318);
+// console.log('rotatedBlocks', rotatedBlocks[0].Geometry.Polygon);
+
+module.exports = {
+  rotate,
+}
