e8009fcade5d621943758c76461d185cd15ea7f0
Oskar Kosowski <okosowski@motorway.co.uk>
2022-06-09 21:29:52 +0200

    docs details and docs list, new backend IP

---
+++ frontend/src/documentDetails/documentDetails.tsx
@@ -0,0 +1,44 @@
+import React, {useEffect, useState} from 'react';
+import {ImagesService} from "../services/images.service";
+import {IDocumentDetails} from "./documentDetails.model";
+
+interface IDocumentDetailsProps {
+    id: string;
+    url: string
+    back: () => void
+}
+
+const DocumentDetails = (props: IDocumentDetailsProps) => {
+    const [document, setDocument] = useState<IDocumentDetails[]>([]);
+    const imagesService: ImagesService = new ImagesService()
+
+    useEffect(() => {
+        imagesService.getSingle(props.id)
+            .then((r: any) => {
+                console.log('r', r);
+                setDocument(r.data)
+            })
+            .catch((e: any) => {
+                console.log('e', e);
+            });
+    }, [props.id])
+
+    if (!document) return null
+
+    return (
+        <div>
+            <button onClick={() => {props.back()}}>back to the list</button>
+            {document.map((single: IDocumentDetails, index: number) => (
+                <div>
+                    <h2>{index + 1}</h2>
+                    <p>{JSON.stringify(single)}</p>
+                    {props.url && (
+                        <img src={props.url} alt=""/>
+                    )}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default DocumentDetails;
---
+++ frontend/src/documentsList/documentsList.tsx
@@ -0,0 +1,45 @@
+import React, {useEffect, useState} from 'react';
+import {IUploadedDocumentOnList} from "./uploadedDocument.model";
+import {ImagesService} from "../services/images.service";
+
+interface IDocumentsListProps {
+    goToDetails: (item: IUploadedDocumentOnList) => void;
+}
+
+const DocumentsList = (props: IDocumentsListProps) => {
+    const [documents, setDocuments] = useState<IUploadedDocumentOnList[]>([]);
+    const imagesService: ImagesService = new ImagesService()
+
+    useEffect(() => {
+        imagesService.getList()
+            .then((r: any) => {
+                setDocuments(r.data)
+                console.log('r', r);
+            })
+            .catch((e: any) => {
+                console.log('e', e);
+            });
+    }, [])
+
+
+    return (
+        <div>
+            <ul>
+                {documents.map((item: any, index: number) => (
+                    <li
+                        key={`${item.dataId}-${index}`}
+                        onClick={() => {
+                            if (item.dataId) {
+                                props.goToDetails(item)
+                            }
+                        }}
+                    >
+                        {index + 1} - {item.dataId} {!item.dataId ? 'processing' : ''}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default DocumentsList;
