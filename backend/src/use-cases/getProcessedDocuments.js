c7f851d2f09dfbcb3fbf2a6fa98d99f7bc49a63b
krzys1u <krzysztof@motorway.co.uk>
2022-06-09 19:34:52 +0200

    api

--- backend/src/use-cases/getProcessedDocuments.js
+++ backend/src/use-cases/getProcessedDocuments.js
@@ -5,12 +5,35 @@ const s3 = new S3({
     region: 'eu-west-1'
 });
 
+const bucketUrl = 'https://document-crackers-2022.s3.eu-west-1.amazonaws.com/'
+
 const listProcessedDocuments = async () => {
     const processedObjectList = await s3.listObjects({
         Bucket: 'document-crackers-2022',
         Prefix: 'images-data/'
     }).promise();
-    return processedObjectList.Contents.splice(1).map((obj) => obj.Key);
+
+    const imagesList = await s3.listObjects({
+        Bucket: 'document-crackers-2022',
+        Prefix: 'images/'
+    }).promise();
+
+    const [images, objects] = await Promise.all([imagesList, processedObjectList]);
+
+    const imagesData = images.Contents.splice(1).map((obj) => obj.Key)
+    const objectsData = objects.Contents.splice(1).map((obj) => obj.Key)
+
+    const ret = imagesData.map((image) => {
+        const dataFileName = image.replace('.','-') + '.json';
+
+        return {
+           imageFile: `${bucketUrl}images/${image}`,
+           dataFile: `${bucketUrl}images-data/${objectsData.includes(dataFileName) ? dataFileName : null}`;
+        }
+    });
+
+    //return processedObjectList.Contents.splice(1).map((obj) => obj.Key);
+    return ret
 }
 
 const getProcessedDocument = async (documentId) => {
