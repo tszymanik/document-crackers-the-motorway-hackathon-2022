ecb01fd4bb6a8b61286ef1422a06bdc734767365
Michał Góra <michal@motorway.co.uk>
2022-06-09 17:11:47 +0200

    Add configs.

---
+++ backend/textract/analysis/imageConfigs.js
@@ -0,0 +1,113 @@
+const v5ImageConfigPage1 = {
+  kind: 'v5',
+  page: 1,
+  staticValues: [
+    {
+      text: 'UNITED KINGDOM',
+      left: 0.5833650231361389,
+      top: 0.033549413084983826,
+
+    },
+    {
+      text: "THIS DOCUMENT IS NOT PROOF OF OWNERSHIP",
+      left: 0.12012216448783875,
+      top: 0.3365984559059143,
+    },
+    {
+      text: "Buyer beware",
+      left: 0.5247732400894165,
+      top: 0.26515424251556396
+    },
+  ],
+  dynamicValues: [
+    {
+      fieldName: 'fullName',
+      left: 0.12786637246608734,
+      top: 0.17554330825805664
+    },
+    {
+      fieldName: 'licenceNumber',
+      left: 0.366097092628479,
+      top: 0.0653751939535141
+    }
+  ]
+};
+
+const v5ImageConfigPage2 = {
+  kind: 'v5',
+  page: 2,
+  staticValues: [
+    {
+      text: 'Vehicle details',
+      left: 0.29780763387680054,
+      top: 0.059533338993787766,
+    },
+    {
+      text: 'Change my vehicle details',
+      left: 0.2956525385379791,
+      top: 0.4300755560398102,
+    },
+    {
+      text: 'Selling or transferring my vehicle to a new keeper',
+      left: 0.2902834713459015,
+      top: 0.6145002841949463,
+    },
+  ],
+  dynamicValues: [
+    {
+      fieldName: 'WVWZZZ6RZCY258332',
+      left: 0.5917980074882507,
+      top: 0.09634590893983841,
+    },
+    {
+      fieldName: 'LB59 WCC',
+      left: 0.3427649736404419,
+      top: 0.08099092543125153,
+    },
+    {
+      fieldName: 'LB59 WCC',
+      left: 0.34271612763404846,
+      top: 0.48084038496017456,
+    },
+    {
+      fieldName: 'LB59 WCC',
+      left: 0.5506651401519775,
+      top: 0.9176637530326843,
+    },
+  ]
+};
+
+const v5ImageConfigPage3 = {
+  kind: 'v5',
+  page: 3,
+  staticValues: [
+    {
+      text: 'Change my name and',
+      left: 0.26986145973205566,
+      top: 0.04208293929696083,
+    },
+    {
+      text: '4 Selling, transferring or part exchanging',
+      left: 0.26626279950141907,
+      top: 0.31712010502815247,
+    },
+    {
+      text: 'New keeper',
+      left: 0.2767312526702881,
+      top: 0.6941570043563843,
+    },
+  ],
+  dynamicValues: [
+    {
+      fieldName: 'WVWZZZ6RZCY258332',
+      left: 0.5917980074882507,
+      top: 0.09634590893983841,
+    },
+  ]
+};
+
+module.exports = {
+  v5ImageConfigPage1,
+  v5ImageConfigPage2,
+  v5ImageConfigPage3,
+}
